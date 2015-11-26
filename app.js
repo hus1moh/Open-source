d8f563ff3c5062701ec9d77970142b69bc76636e
sigurdtk <sigurdtk15@gmail.com>
2015-11-26 09:35:37 +0100

    Trial

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+$scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+];
+function($scope){
+  $scope.test = 'Hello world!';
+}]);
